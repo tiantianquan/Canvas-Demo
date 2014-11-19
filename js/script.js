2f9e9f6984fe252d96da68c316548a95d88665fe
tiantianquan <fenqiufen@gmail.com>
2014-11-19 23:50:28 +0800

    模块化

--- js/script.js
+++ js/script.js
@@ -9,50 +9,74 @@ ctx.clean = function() {
   ctx.fillRect(0, 0, c.width, c.height)
 }
 
-var CircleDrew = function(x, y, radii) {
-  this.x = x
-  this.y = y
-  this.radii = radii
-  this.endAngle = 0
+var CircleDrew = (function() {
+  var circleDrew = function(x, y, radii) {
+    this.x = x
+    this.y = y
+    this.radii = radii
+    this.endAngle = 0
 
-  CircleDrew.list.push(this)
-  this.tween()
-}
-
-CircleDrew.prototype.tween = function() {
-  var that = this
-  TweenLite.to(that, 2, {
-    endAngle: 2 * Math.PI,
-    ease: Linear.easeNone,
-    onComplete: function() {},
-  })
-}
-
-CircleDrew.prototype.update = function() {
-  ctx.beginPath()
-  ctx.arc(this.x, this.y, this.radii, 0, this.endAngle)
-  ctx.strokeStyle = 'red'
-  ctx.stroke()
-};
-
-CircleDrew.list = []
-CircleDrew.update = function() {
-  CircleDrew.list.forEach(function(cd) {
-    cd.update()
-  })
-}
-
-function beginAnimate() {
-  for (var i = 0; i < 10; i++) {
-    var cd = new CircleDrew(c.width / 2, c.height / 2, 10 * i)
+    this.list.push(this)
   }
+
+  circleDrew.list = circleDrew.prototype.list = []
+
+  circleDrew.updateAll = circleDrew.prototype.updateAll = function() {
+    circleDrew.list.forEach(function(cd) {
+      cd.update()
+    })
+    return this
+  }
+
+  circleDrew.prototype.tween = function() {
+    var that = this
+    TweenLite.to(that, that._tweenDur, {
+      endAngle: 2 * Math.PI,
+      ease: Linear.easeNone,
+      onComplete: that._tweenOnComplete,
+    })
+    return this
+  }
+  circleDrew.prototype.tweenDur = function(dur) {
+    this._tweenDur = dur
+    return this
+  }
+  circleDrew.prototype.tweenOnComplete = function(onComplete) {
+    var that = this
+    this._tweenOnComplete = function() {
+      onComplete(that)
+    }
+    return this
+  }
+
+  circleDrew.prototype.update = function() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radii, 0, this.endAngle)
+    ctx.strokeStyle = 'red'
+    ctx.stroke()
+    return this
+  };
+
+  return circleDrew
+})()
+
+function main() {
+  for (var i = 1; i <= 100; i++) {
+    var cd = new CircleDrew(c.width / 2, c.height / 2, 10 * i)
+    cd.tweenDur(.5).tweenOnComplete(function(self) {
+      var next = self.list[self.list.indexOf(self) + 1]
+      if (!!next)
+        next.tween()
+    })
+  }
+  cd.list[0].tween()
 
   function loop() {
     ctx.clean()
-    CircleDrew.update()
+    cd.updateAll()
     window.requestAnimationFrame(loop)
   }
   loop()
 }
 
-beginAnimate()
+main()
