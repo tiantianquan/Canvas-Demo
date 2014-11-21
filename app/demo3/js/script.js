bec949912e75c0394d64e450f623de817a38e5b4
tiantianquan <fenqiufen@gmail.com>
2014-11-21 12:13:32 +0800

    优化Demo3

--- app/demo3/js/script.js
+++ app/demo3/js/script.js
@@ -35,6 +35,11 @@ var Octagon = (function() {
       o.createPoint()
     })
   }
+  _octagon.prototype.rotate = function(r) {
+    return gs.call(this, arguments, '_rotate', function(o) {
+      o.createPoint()
+    })
+  }
   _octagon.prototype.createPoint = function() {
     if (!this._long || !this._center)
       return
@@ -42,6 +47,15 @@ var Octagon = (function() {
     var long = this._long
     var Rx = long / 2
     var Ry = Rx / Math.tan(22.5 / 180 * Math.PI)
+
+    // if (this._rotate) {
+    //   var x = Rx
+    //   var y = Ry
+    //   var that = this
+    //   Rx = x * Math.cos(-that._rotate) - y * Math.sin(-that._rotate);
+    //   Ry = y * Math.cos(-that._rotate) + x * Math.sin(-that._rotate);
+    // }
+
     var radii = Rx / Math.sin(22.5 / 180 * Math.PI)
     var x = this._center[0]
     var y = this._center[1]
@@ -54,8 +68,24 @@ var Octagon = (function() {
     this._point[5] = this._p6 = [x - Rx, y - Ry]
     this._point[6] = this._p7 = [x - Ry, y - Rx]
     this._point[7] = this._p8 = [x - Ry, y + Rx]
+
+    if (this._rotate) {
+      var that = this
+      this._point.forEach(function(p, index, list) {
+        // var x = that._center[0]
+        // var y = that._center[1]
+        var lx = p[0] - x
+        var ly = p[1] - y
+        var _lx = lx * Math.cos(-that._rotate) - ly * Math.sin(-that._rotate);
+        var _ly = ly * Math.cos(-that._rotate) + lx * Math.sin(-that._rotate);
+        p[0] = _lx + x
+        p[1] = _ly + y
+      })
+    }
+
   }
   _octagon.prototype.drew = function() {
+    // ctx.rotate(0.1 * 2 * Math.PI)
     ctx.beginPath()
     ctx.moveTo(this._p1[0], this._p1[1])
     for (var i = 1; i < 8; i++) {
@@ -66,20 +96,43 @@ var Octagon = (function() {
     ctx.strokeStyle = '#000'
     ctx.stroke()
   }
-
   return _octagon
 })()
 
 function main() {
-  var long = 200
-  var o = new Octagon().long(long).center([c.width/2, c.height/2])
-  console.log(o)
+  var tween = {
+    long: 200,
+    centerX: c.width / 2,
+    centerY: c.height / 2,
+    rotate: 0,
+  }
+
+  tween.go = function() {
+    tween.rotate = 0
+    TweenLite.to(tween, 1, {
+      rotate: 2 * Math.PI,
+      onUpdate: function() {
+        o.rotate(tween.rotate)
+        o1.rotate(-tween.rotate)
+      },
+      onComplete: tween.go,
+      ease: Linear.easeNone
+    })
+  }
+
+  var o = new Octagon().long(tween.long*.21).center([c.width / 2, c.height / 2])
+    .rotate(.9 * 2 * Math.PI)
+
+  var o1 = new Octagon().long(tween.long*.2).center([c.width / 2, c.height / 2])
+    .rotate(-0.5 * 2 * Math.PI)
+
+  tween.go()
 
   function loop() {
-    ctx.clean()
-    o.long(long)
+    // ctx.clean()
+    ctx.clearRect(0, 0, c.width, c.height)
     o.drew()
-    long--
+    o1.drew()
     window.requestAnimationFrame(loop)
   }
   loop()
