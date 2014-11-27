0a7ba3e027ac9e067ec0b8437063d7f3df00cbe7
tiantianquan <fenqiufen@gmail.com>
2014-11-27 14:19:25 +0800

    demo5

--- app/demo5/js/script.js
+++ app/demo5/js/script.js
@@ -2,12 +2,17 @@ var c = document.querySelector('#c')
 var ctx = c.getContext('2d')
 c.width = window.innerWidth
 c.height = window.innerHeight
+ctx.globalCompositeOperation = 'lighter'
 
 var tool = {
   //两点距离
   pointDistance: function(x1, y1, x2, y2) {
     return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
+  },
+  getRandomInt: function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
+
 }
 var Ball = function(opt) {
   var opt = opt || {}
@@ -18,6 +23,7 @@ var Ball = function(opt) {
   this.strokecolor = opt.strokecolor || '#fff'
 
   this.moveTween
+
 
   var canv = this.canv = opt.canvas || c
 
@@ -47,11 +53,10 @@ var Ball = function(opt) {
     $(canv).on('mousedown', function(e) {
       if (isInBall.call(that, e.pageX, e.pageY))
         that.changeColor('red')
-      if (!!that.moveTween)
-        that.moveTween.kill()
+
       $(canv).on('mousemove', function(e) {
-        // that.cx = e.pageX
-        // that.cy = e.pageY
+        if (!!that.moveTween)
+          that.moveTween.kill()
         that.move(e.pageX, e.pageY)
       })
 
@@ -69,7 +74,8 @@ var Ball = function(opt) {
 Ball.list = Ball.prototype.list = []
 
 Ball.prototype.move = function(targetX, targetY) {
-  this.moveTween = TweenLite.to(this, 2, {
+  var that = this
+  this.moveTween = TweenLite.to(this, 5, {
     cx: targetX,
     cy: targetY,
   })
@@ -86,48 +92,78 @@ Ball.prototype.drew = function() {
   ctx.fill()
 }
 
-Ball.check = function() {
+Ball.prototype.crash = function(otherball) {
+  var distance = this.radii * 20
+  var angle = Math.asin((otherball.cy - this.cy) / (this.radii + otherball.radii))
+  var mx = distance * Math.cos(angle + Math.random() * 0.1 * Math.PI)
+  var my = distance * Math.sin(angle + Math.random() * 0.1 * Math.PI)
+  mx = this.cx < otherball.cx ? mx : -mx
+  this.moveTween.kill()
+  this.move(this.cx - mx, this.cy - my)
+}
+
+Ball.prototype.crashWall = function() {
+  var distance = this.radii * 20
+  var mx = distance * Math.cos(Math.random() * 2 * Math.PI)
+  var my = distance * Math.sin(Math.random() * 2 * Math.PI)
+  this.moveTween.kill()
+  this.move(this.cx - mx, this.cy - my)
+}
+
+Ball.prototype.check = Ball.check = function() {
   var that = this
   this.list.forEach(function(ball, index, list) {
-    // for (var i = 1; i < list.length - index; i++) {
-    //   var long = tool.pointDistance(ball.cx, ball.cy, list[list.length - i - 1].cx, list[list.length - i - 1].cy)
-    //   console.log(that.radii)
-    //   if (long <= 2 * 50) {
-    //     if (!!ball.moveTween)
-    //       ball.moveTween.kill()
-    //   }
-    // }
-    if(!list[index + 1])
-      return 
-    var long = tool.pointDistance(ball.cx, ball.cy, list[index + 1].cx, list[index + 1].cy)
-    if (long <= 2 * 50) {
-      if (!!ball.moveTween){
-        list[index + 1].moveTween.kill()
-        ball.moveTween.kill()
-        ball.move(ball.cx-100,ball.cy-100)
-        list[index + 1].move(list[index + 1].cx+100,list[index + 1].cy+100)
+    var i = index + 1
+    while (i <= list.length - 1) {
+      if (!list[i])
+        return
+      var otherBall = list[i]
+        //球体碰撞
+      var long = tool.pointDistance(ball.cx, ball.cy, otherBall.cx, otherBall.cy)
+      if (long <= 10/*ball.radii + otherBall.radii */) {
+        if (!!ball.moveTween) {
+          ball.crash(otherBall)
+          otherBall.crash(ball)
+        }
+      }
+      i++
+    }
+
+    if (ball.cx - ball.radii <= 0 ||
+      ball.cx + ball.radii >= ball.canv.width ||
+      ball.cy + ball.radii >= ball.canv.height ||
+      ball.cy - ball.radii <= 0) {
+      if (!!ball.moveTween) {
+        ball.crashWall()
       }
     }
+
   })
 }
 
+Ball.isCheck = true
+
 
 function main() {
-  new Ball({
-    radii: 50,
-    cx: 100,
-    cy: 100
-  })
-  new Ball({
-    radii: 50,
-    cx: 200,
-    cy: 200,
-    fillcolor: '#000'
-  })
+  for (var i = 0; i < 100; i++) {
+    new Ball({
+      radii: 30,
+      cx: tool.getRandomInt(c.width),
+      cy: tool.getRandomInt(c.height),
+      fillcolor: 'rgba(' + tool.getRandomInt(0, 255) + ',' + tool.getRandomInt(0, 255) + ',' + tool.getRandomInt(0, 255) + ',0.3)'
+    })
+  }
 
   function loop() {
     ctx.clearRect(0, 0, c.width, c.height)
-    Ball.check()
+    $(window).on('mouseup', function() {
+      Ball.isCheck = true
+    })
+    $(c).on('mousedown', function() {
+      Ball.isCheck = false
+    })
+    if (Ball.isCheck)
+      Ball.check()
     Ball.list.forEach(function(ball) {
       ball.drew()
     })
