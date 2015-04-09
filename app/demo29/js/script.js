10d867224d32e884e33708c9a18197c3d293d3ac
tiantianquan <fenqiufen@gmail.com>
2015-04-09 16:19:22 +0800

    demo29

--- app/demo29/js/script.js
+++ app/demo29/js/script.js
@@ -96,14 +96,128 @@ ctx.fillRect(0, 0, c.width, c.height)
 //   ctx.stroke()
 // })
 
-var LowPoly = function(opt){
+var LowPoly = function(opt) {
   this.opt = opt || {}
   this.gap = opt.gap
-  this.cw = opt.cw
-  this.ch = opt.ch
+  this.cw = opt.cw || cw
+  this.ch = opt.ch || ch
+  this.pointStyle = opt.pointStyle
+  this.lineStyle = opt.lineStyle
+  this.pointRadii = opt.pointRadii
+  this.lineWidth = opt.lineWidth
   this.points = []
+  this._points = []
+
+  this.getPoints()
 }
 
-LowPoly.prototype.getPoints = function(){
-  var x = this.cw/this.gap
+LowPoly.prototype.getPoints = function() {
+  var startX = (this.cw % this.gap) / 2
+  var startY = (this.ch % this.gap) / 2
+  var numX = Math.round(this.cw / this.gap)
+  var numY = Math.round(this.ch / this.gap)
+
+  for (var i = 0; i <= numX; i++) {
+    var verticalList = []
+    for (var j = 0; j <= numY - 1; j++) {
+      var point = {}
+      point.x = startX + this.gap * i
+      point.y = startY + this.gap * j
+      verticalList.push(point)
+    }
+    this.points.push(verticalList)
+  }
+
 }
+
+LowPoly.prototype.movePoints = function(moveDis) {
+  var gap = this.gap
+  var points = this.points
+
+  for (var i = 0; i < points.length; i++) {
+    for (var j = 0; j < points[i].length; j++) {
+      this.randomPoint(points[i][j], [-moveDis, moveDis])
+    }
+  }
+}
+
+LowPoly.prototype.randomPoint = function(point, dis) {
+  var randomX, randomY
+  randomX = util.random(dis)
+  randomY = util.random(dis)
+  point.x += randomX
+  point.y += randomY
+}
+
+LowPoly.prototype.drawPoint = function() {
+  var color = this.pointStyle
+  var radii = this.pointRadii
+
+  ctx.fillStyle = color
+  this.points.forEach(function(vl) {
+    vl.forEach(function(p) {
+      ctx.beginPath()
+      ctx.arc(p.x, p.y, radii, 0, Math.PI * 2, false)
+      ctx.fill()
+    })
+  })
+}
+
+LowPoly.prototype.drawLine = function(isdiagonal) {
+  ctx.strokeStyle = this.lineStyle
+  ctx.lineWidth = this.lineWidth
+  var points = this.points
+
+  ctx.beginPath()
+  for (var i = 0; i < points.length; i++) {
+    for (var j = 0; j < points[i].length; j++) {
+      if (j === 0)
+        ctx.moveTo(points[i][j].x, points[i][j].y)
+      else
+        ctx.lineTo(points[i][j].x, points[i][j].y)
+    }
+  }
+
+  for (var j = 0; j < points[0].length; j++) {
+    for (var i = 0; i < points.length; i++) {
+      if (i === 0)
+        ctx.moveTo(points[i][j].x, points[i][j].y)
+      else
+        ctx.lineTo(points[i][j].x, points[i][j].y)
+    }
+  }
+
+  if (isdiagonal === true) {
+    for (var i = 0; i + 1 < points.length; i++) {
+      for (var j = 0; j + 1 < points[i].length; j++) {
+        ctx.moveTo(points[i][j + 1].x, points[i][j + 1].y)
+        ctx.lineTo(points[i + 1][j].x, points[i + 1][j].y)
+      }
+    }
+  }
+  ctx.stroke()
+}
+
+var lowP = new LowPoly({
+  lineStyle: '#000',
+  lineWidth: 0.5,
+  pointStyle: '#000',
+  pointRadii: 2,
+  gap: 50,
+})
+
+
+
+function draw() {
+  lowP.movePoints(1)
+  lowP.drawPoint()
+  lowP.drawLine(true)
+}
+
+
+
+c.addEventListener('mousemove', function(e) {
+  ctx.fillStyle = '#eee'
+  ctx.fillRect(0, 0, cw, ch)
+  draw()
+})
