af7fa4f2f37e0c7d2f7a5413e1f4d610099f1a1d
tiantianquan <fenqiufen@gmail.com>
2015-05-19 23:29:03 +0800

    no message

--- app/demo33/js/script.js
+++ app/demo33/js/script.js
@@ -1,5 +1,9 @@
-var canvas = document.getElementById('c');
-gl = initWebGL(canvas)
+var c = document.getElementById('c')
+var cw = window.innerWidth
+var ch = window.innerHeight
+c.height = ch
+c.width = cw
+gl = initWebGL(c)
 
 function initWebGL(canvas) {
   gl = null;
@@ -20,46 +24,49 @@ function initWebGL(canvas) {
 
 //--------------
 
-var vertexBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-var vertices = [-0.5, -0.5, 0.5, -0.5, 0, 0.5];
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices),gl.STATIC_DRAW);
-
-//setup for the vertex shader and fragment shader
-var vs = 'attribute vec2 pos;' +
-'void main(){ gl_Position = vec4(pos,0,1);}';
-var fs = 'precision mediump float;' +
-'void main(){ gl_FragColor = vec4(0,0.8,0,1);}';
 
 
-function createShader(str, type){
-  var shader = gl.createShader(type);
-  gl.shaderSource(shader,str);
-  gl.compileShader(shader);
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    throw gl.getShaderInfoLog(shader);
-  }
-  return shader;
-}
-function createProgram(vstr,fstr){
-  var program = gl.createProgram();
-  var vshader = createShader(vstr,gl.VERTEX_SHADER);
-  var fshader = createShader(fstr, gl.FRAGMENT_SHADER);
-  gl.attachShader(program,vshader);
-  gl.attachShader(program,fshader);
-  gl.linkProgram(program);
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    throw gl.getProgramInfoLog(program);
-  }
-  return program;
-}
-//创建shader program
-var program = createProgram(vs,fs);
-gl.useProgram(program);
 
-program.vertexPosAttrib = gl.getAttribLocation(program,'pos');
-gl.enableVertexAttribArray(program.vertexPosAttrib);
-gl.vertexAttribPointer(program.vertexPosAttrib, 2, gl.FLOAT, false, 0, 0);
+// var vertexBuffer = gl.createBuffer();
+// gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+// var vertices = [-0.5, -0.5, 0.5, -0.5, 0, 0.5];
+// gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices),gl.STATIC_DRAW);
+
+// //setup for the vertex shader and fragment shader
+// var vs = 'attribute vec2 pos;' +
+// 'void main(){ gl_Position = vec4(pos,0,1);}';
+// var fs = 'precision mediump float;' +
+// 'void main(){ gl_FragColor = vec4(0,0.8,0,1);}';
 
 
-gl.drawArrays(gl.TRIANGLES,0,3);
+// function createShader(str, type){
+//   var shader = gl.createShader(type);
+//   gl.shaderSource(shader,str);
+//   gl.compileShader(shader);
+//   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+//     throw gl.getShaderInfoLog(shader);
+//   }
+//   return shader;
+// }
+// function createProgram(vstr,fstr){
+//   var program = gl.createProgram();
+//   var vshader = createShader(vstr,gl.VERTEX_SHADER);
+//   var fshader = createShader(fstr, gl.FRAGMENT_SHADER);
+//   gl.attachShader(program,vshader);
+//   gl.attachShader(program,fshader);
+//   gl.linkProgram(program);
+//   if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+//     throw gl.getProgramInfoLog(program);
+//   }
+//   return program;
+// }
+// //创建shader program
+// var program = createProgram(vs,fs);
+// gl.useProgram(program);
+
+// program.vertexPosAttrib = gl.getAttribLocation(program,'pos');
+// gl.enableVertexAttribArray(program.vertexPosAttrib);
+// gl.vertexAttribPointer(program.vertexPosAttrib, 2, gl.FLOAT, false, 0, 0);
+
+
+// gl.drawArrays(gl.TRIANGLES,0,3);
